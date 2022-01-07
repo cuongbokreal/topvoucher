dde47c712570cc55290a872c1d74aa9ce75e6c1b
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-01-07 14:38:45 +0700

    Create data-voucher.js

---
+++ js/data-voucher.js
@@ -0,0 +1,5 @@
+var mainVoucher = document.getElementById('main-voucher');
+var headingImg = document.getElementById('heading-img');
+
+var innerHeadingImg = '';
+if (innerHeadingImg.length > 3){headingImg.src = innerHeadingImg}
