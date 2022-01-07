93b19bd6aa57b58203726ac47fbc316dba0a1f4d
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-01-07 14:40:31 +0700

    Update data-voucher.js

--- js/data-voucher.js
+++ js/data-voucher.js
@@ -1,5 +1,10 @@
+var innerMainVoucher = ``;
+
 var mainVoucher = document.getElementById('main-voucher');
 var headingImg = document.getElementById('heading-img');
 
 var innerHeadingImg = '';
 if (innerHeadingImg.length > 3){headingImg.src = innerHeadingImg}
+
+
+if (innerMainVoucher.length > 3){mainVoucher.innerHTML = innerMainVoucher}
