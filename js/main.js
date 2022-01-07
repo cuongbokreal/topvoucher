20cbbcb14fa09d00c069e5721f6fe79d8f2b7b97
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-01-07 15:09:43 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 var updateTime = new Date(); //updateTime
+var updateDate = updateTime.getDate();
+var updateMonth = updateTime.getMonth() + 1;
 
 var date = document.querySelector('#date');
 date.innerText = `${updateTime.getDate()}.${updateTime.getMonth() + 1}`
