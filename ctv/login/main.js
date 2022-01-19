da4938f20c27a2df0813f9bf1a7f5ff6a572c443
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-01-19 18:52:26 +0700

    Update main.js

--- ctv/login/main.js
+++ ctv/login/main.js
@@ -5,9 +5,7 @@ function login(){
   var loginTk = document.getElementById('loginTk').value;
   var loginMk = document.getElementById('loginMk').value;
   
-  for(let i=0; i<dataUser.length; i++){
-    if(loginTk == dataUser[i].tk && loginMk == dataUser[i].mk){
+    if(loginTk == dataUser[loginTk].tk && loginMk == dataUser[loginTk].mk){
       window.location.href = `././report/${loginTk}/?ctv=true`;
     }else{thbao.innerHTML = `Sai id hoặc mật khẩu!`}
-  }
 }
