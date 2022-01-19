9e0b26f6f6f4bb10462306ca291367721c72effc
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-01-19 18:38:39 +0700

    Update data.js

--- ctv/login/data.js
+++ ctv/login/data.js
@@ -1,6 +1,16 @@
 var dataUser = [
   {"id":"00",
    "name":"TrumGiamGia",
-   "":"",
+   "tk":"00",
+   "mk":"00",
+   "idGioiThieu":"",
+   "idNgGioiThieu":""
+  },
+  {"id":"01",
+   "name":"Vinh",
+   "tk":"01",
+   "mk":"00",
+   "idGioiThieu":"00",
+   "idNgGioiThieu":""
   },
 ]
