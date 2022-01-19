82ae8b58e589a5de439f684e94fba960aa3f03d9
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-01-19 18:58:30 +0700

    Update main.js

--- ctv/login/main.js
+++ ctv/login/main.js
@@ -1,11 +1,31 @@
-var thbao = document.getElementById('thbao').value;
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+ 
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};
+var a = getUrlParameter('a');
 
+var thbao = document.getElementById('thbao').value;
+var loginTk = document.getElementById('loginTk');
+var loginMk = document.getElementById('loginMk');
 
 function login(){
-  var loginTk = document.getElementById('loginTk').value;
-  var loginMk = document.getElementById('loginMk').value;
-  
-    if(loginTk == dataUser[loginTk].tk && loginMk == dataUser[loginTk].mk){
-      window.location.href = `././report/${loginTk}/?ctv=true`;
-    }else{thbao.innerHTML = `Sai id hoặc mật khẩu!`}
+
+  if(a == 'report'){loginAction = `./report/${parseInt(tk.value)}?ctv=true`;}else
+  if(a == 'payment'){loginAction = `./payment/${parseInt(tk.value)}?ctv=true`;}
+  //look tk
+  if(dataUser[parseInt(loginTk.value)].mk == loginMk.value){
+    document.getElementById('thbao').innerHTML = 'Đăng nhập thành công!'
+    window.location.replace(loginAction);
+  }else{
+    document.getElementById('thbao').innerHTML = 'Sai thông tin đăng nhập, vui lòng nhập lại!'
+  }
 }
