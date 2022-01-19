93f6ce67822a70ffc8314fcc53fdf58a7c9e7872
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-01-19 18:47:29 +0700

    Update main.js

--- ctv/login/main.js
+++ ctv/login/main.js
@@ -1,2 +1,13 @@
-var loginTk = document.getElementById('loginTk');
-var loginMk = document.getElementById('loginMk');
+
+
+
+function login(){
+  var loginTk = document.getElementById('loginTk').value;
+  var loginMk = document.getElementById('loginMk').value;
+  
+  for(let i=0; i<dataUser.length; i++){
+    if(loginTk = dataUser[i].tk && loginMk = dataUser[i].mk){
+      window.location.href = `././report/${loginTk}/?ctv=true`;
+    }
+  }
+}
