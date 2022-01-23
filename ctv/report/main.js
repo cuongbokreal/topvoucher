a749269ee2204c207c2355dee9cd15b6e3072e71
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-01-23 17:47:15 +0700

    Update main.js

--- ctv/report/main.js
+++ ctv/report/main.js
@@ -1,5 +1,21 @@
 var innerDoanhThu = document.getElementById('innerDoanhThu');
+let dataInnerDoanhThu = "";
+for(let i=0; i<dataDoanhThu.length;i++){
+  dataInnerDoanhThu += `<tr>
+                <th>${dataDoanhThu[i].nhaQuangCao}</th>
+                <th>${dataDoanhThu[i].giaTriDonHang}</th>
+                <th>${dataDoanhThu[i].hoaHong}</th>
+                <th>${dataDoanhThu[i].duocThanhToan}</th>
+                <th>${dataDoanhThu[i].daHuy}</th>
+                <th>${dataDoanhThu[i].choXuly}</th>
+              </tr>`;
+}
+innerDoanhThu.innerHTML = dataInnerDoanhThu;
+
 var innerThanhToan = document.getElementById('innerThanhToan');
+
+
+
 
 function doanhthu(){
   document.getElementById("doanhthu").className = "px-3 border-b-2 border-blue-500 text-blue-500 dark:text-white dark:border-white pb-1.5";
