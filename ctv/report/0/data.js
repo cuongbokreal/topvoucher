c526bb09c6b24e49b98b96a400d39f3fd0b5a737
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-01-23 18:14:17 +0700

    Update data.js

--- ctv/report/0/data.js
+++ ctv/report/0/data.js
@@ -3,27 +3,27 @@ var pubName = 'Admin';
 var dataDoanhThu = [
   {
    "nhaQuangCao":"Shopee",
-   "giaTriDonHang": 0 ,
-   "hoaHong": 0 ,
+   "giaTriDonHang": 105922 ,
+   "hoaHong": 15341 ,
    "duocThanhToan": 0 ,
    "daHuy": 0 ,
    "choXuly": 0 ,
   },
   {
    "nhaQuangCao":"Lazada",
-   "giaTriDonHang": 0 ,
-   "hoaHong": 0 ,
-   "duocThanhToan": 0 ,
-   "daHuy": 0 ,
+   "giaTriDonHang": 989451 ,
+   "hoaHong": 15123 ,
+   "duocThanhToan": 4641 ,
+   "daHuy": 26 ,
    "choXuly": 0 ,
   },
   {
    "nhaQuangCao":"Tiki",
-   "giaTriDonHang": 0 ,
-   "hoaHong": 0 ,
-   "duocThanhToan": 0 ,
-   "daHuy": 0 ,
-   "choXuly": 0 ,
+   "giaTriDonHang": 949456 ,
+   "hoaHong": 54012 ,
+   "duocThanhToan": 151 ,
+   "daHuy": 554 ,
+   "choXuly": 5464 ,
   },
 ];
 
