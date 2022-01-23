1ec84f8656ece784c8590e937afc5c01cc36fdc2
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-01-23 18:03:25 +0700

    Update data.js

--- ctv/report/0/data.js
+++ ctv/report/0/data.js
@@ -2,35 +2,35 @@ var pubName = 'Admin';
 
 var dataDoanhThu = [
   {
-    "nhaQuangCao":"Shopee",
-   "giaTriDonHang":"0",
-   "hoaHong":"0",
-   "duocThanhToan":"0",
-   "daHuy":"0",
-   "choXuly":"0",
+   "nhaQuangCao":"Shopee",
+   "giaTriDonHang": 0 ,
+   "hoaHong": 0 ,
+   "duocThanhToan": 0 ,
+   "daHuy": 0 ,
+   "choXuly": 0 ,
   },
   {
-    "nhaQuangCao":"Lazada",
-   "giaTriDonHang":"0",
-   "hoaHong":"0",
-   "duocThanhToan":"0",
-   "daHuy":"0",
-   "choXuly":"0",
+   "nhaQuangCao":"Lazada",
+   "giaTriDonHang": 0 ,
+   "hoaHong": 0 ,
+   "duocThanhToan": 0 ,
+   "daHuy": 0 ,
+   "choXuly": 0 ,
   },
   {
-    "nhaQuangCao":"Tiki",
-   "giaTriDonHang":"0",
-   "hoaHong":"0",
-   "duocThanhToan":"0",
-   "daHuy":"0",
-   "choXuly":"0",
+   "nhaQuangCao":"Tiki",
+   "giaTriDonHang": 0 ,
+   "hoaHong": 0 ,
+   "duocThanhToan": 0 ,
+   "daHuy": 0 ,
+   "choXuly": 0 ,
   },
 ];
 
 var dataThanhToan = [
   {"thangDoiSoat":"1-2022",
-   "hoaHongDuocDuyet":"0",
-   "daThanhToan":"0",
-   "soDu":"0",
+   "hoaHongDuocDuyet": 0 ,
+   "daThanhToan": 0 ,
+   "soDu": 0 ,
   }
 ]
