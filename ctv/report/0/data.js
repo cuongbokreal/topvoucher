557fc2ebd7a53c66957fca1a0a76abcf0baf898d
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-01-23 17:41:20 +0700

    Create data.js

---
+++ ctv/report/0/data.js
@@ -0,0 +1,28 @@
+var pubName = 'Admin';
+
+var dataDoanhThu = [
+  {
+    "nhaQuangCao":"Shopee",
+   "giaTriDonHang":"0",
+   "hoaHong":"0",
+   "duocThanhToan":"0",
+   "daHuy":"0",
+   "choXuly":"0",
+  },
+  {
+    "nhaQuangCao":"Lazada",
+   "giaTriDonHang":"0",
+   "hoaHong":"0",
+   "duocThanhToan":"0",
+   "daHuy":"0",
+   "choXuly":"0",
+  },
+  {
+    "nhaQuangCao":"0",
+   "giaTriDonHang":"0",
+   "hoaHong":"0",
+   "duocThanhToan":"0",
+   "daHuy":"0",
+   "choXuly":"0",
+  },
+]
