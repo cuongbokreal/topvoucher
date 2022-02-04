c9870a6bf99e7990256e57eed7b802779e207cd5
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-02-04 15:04:56 +0700

    Create data.js

---
+++ ctv/report/1/data.js
@@ -0,0 +1,39 @@
+var pubName = 'Trần Công Vinh';
+
+var dataDoanhThu = [
+  {
+   "nhaQuangCao":"Shopee",
+   "giaTriDonHang": 0 ,
+   "hoaHong": 0 ,
+   "duocThanhToan": 0 ,
+   "daHuy": 0 ,
+   "choXuly": 0 ,
+  },
+  {
+   "nhaQuangCao":"Lazada",
+   "giaTriDonHang": 0 ,
+   "hoaHong": 0 ,
+   "duocThanhToan": 0 ,
+   "daHuy": 0 ,
+   "choXuly": 0 ,
+  },
+  {
+   "nhaQuangCao":"Tiki",
+   "giaTriDonHang": 0 ,
+   "hoaHong": 0 ,
+   "duocThanhToan": 0 ,
+   "daHuy": 0 ,
+   "choXuly": 0 ,
+  },
+];
+
+var dataThanhToan = [
+  {"thangDoiSoat":"1-2022",
+   "hoaHongDuocDuyet": 0 ,
+   "daThanhToan": 0 ,
+  },
+  {"thangDoiSoat":"12-2021",
+   "hoaHongDuocDuyet": 0 ,
+   "daThanhToan": 0 ,
+  },
+]
