75c19ae32e0fe84d8b529977b62d03ba6d4f1b78
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-02-05 19:42:19 +0700

    Update innerHTML.js

--- ctv/report/innerHTML.js
+++ ctv/report/innerHTML.js
@@ -2,7 +2,7 @@ document.getElementById('main').innerHTML = `
 
   <div class="bg-gray-100 dark:bg-gray-900 dark:text-white text-gray-600 h-screen flex overflow-hidden text-sm">
   <div class="flex-grow overflow-hidden h-full flex flex-col">
-    <div id="navmobile">
+    <div id="navmobile" style="display:none">
         <header class="block">
                 <ul class="header-menu horizontal-list">
                     <li>
