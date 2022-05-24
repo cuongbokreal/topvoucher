90030e2b21ad413bd8f8a318efa30e2a0eea62bd
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-05-24 13:42:44 +0700

    Create data.js

---
+++ ctv/report/012405/data.js
@@ -0,0 +1,51 @@
+var pubName = 'Nguyen Van Hieu';
+
+var dataDoanhThu = [
+  {
+   "nhaQuangCao":"Shopee",
+   "chuyenDoiPhatSinh": 0 ,
+   "giaTriDonHang":  0,
+   "hoaHong": 0 ,
+   "duocDuyet": 0 ,
+   "daHuy": 0 ,
+  },
+  {
+   "nhaQuangCao":"Lazada",
+   "chuyenDoiPhatSinh": 0 ,
+   "giaTriDonHang": 0 ,
+   "hoaHong": 0 ,
+   "duocDuyet": 0 ,
+   "daHuy": 0 ,
+  },
+  {
+   "nhaQuangCao":"Tiki",
+   "chuyenDoiPhatSinh": 0 ,
+   "giaTriDonHang": 0 ,
+   "hoaHong": 0 ,
+   "duocDuyet": 0 ,
+   "daHuy": 0 ,
+  },
+  {
+   "nhaQuangCao":"Thưởng thêm (Brand Bonus)",
+   "giaTriDonHang": 0 ,
+   "chuyenDoiPhatSinh": 0 ,
+   "hoaHong": 0 ,
+   "duocDuyet": 0 ,
+   "daHuy": 0 ,
+  },
+  {
+   "nhaQuangCao":"Giới thiệu CTV",
+   "chuyenDoiPhatSinh": 0 ,
+   "giaTriDonHang": 0 ,
+   "hoaHong": 0 ,
+   "duocDuyet": 0 ,
+   "daHuy": 0 ,
+  },
+];
+
+var dataThanhToan = [
+  {"thangDoiSoat":"05-2022",
+   "hoaHongDuocDuyet": 0 ,
+   "daThanhToan": 0 ,
+  },
+]
