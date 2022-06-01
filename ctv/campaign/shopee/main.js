a2ba7a1fc36faec859d3418da403d3ccdfd7cad7
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-06-01 22:50:29 +0700

    Create main.js

---
+++ ctv/campaign/shopee/main.js
@@ -0,0 +1,106 @@
+var camp_name = 'Shopee Việt Nam Smartlink cho tất cả thiết bị ';
+
+var camp_des1 =  `Shopee là website thương mại điện tử C2C hàng đầu Việt Nam
+
+Tổng hợp chương trình lớn tháng 5 của Shopee: <a target="_blank" href="https://docs.google.com/spreadsheets/d/17CT-UJDjBW1NLt2cB4Hp0cqlGOB5uOAYe_g_qC2m95U/edit#gid=691809098">tại đây</a>
+
+Thưởng theo nhãn hàng liên tục cập nhật: <a target="_blank" href="https://docs.google.com/spreadsheets/d/17CT-UJDjBW1NLt2cB4Hp0cqlGOB5uOAYe_g_qC2m95U/edit#gid=1176046430">tại đây</a>
+
+Danh sách sản phẩm bán chạy: <a target="_blank" href="https://docs.google.com/spreadsheets/d/1XaNEEcM-0AdzEnxjiMcCsSHv-j2AxKd5/edit#gid=1137356070">tại đây</a>`;
+
+
+var camp_des2 = `CHƯƠNG TRÌNH THƯỞNG THÁNG 5
+
+Chương trình Shopee Thường Cả Tháng 5
+
+Chương trình thưởng khi tăng trưởng về qualified click trong tháng 5	 
+
+📣 Chương trình 1: thưởng khi tăng trưởng về qualified click trong tháng 5
+
+🔺 Với Publisher đã có đơn trong tháng 4
+🔶 Nếu qualified click tăng 1.5 lần so với tháng 4, bonus 5% trên tổng hoa hồng được duyệt trong tháng 5
+🔶 Nếu qualified click tăng từ 2 lần trở lên so với tháng 4, bonus 7% trên tổng hoa hồng được duyệt trong tháng 5
+🔺 Với Publisher chưa có đơn trong tháng 4
+🔶 Nếu publisher có từ 1000 qualified click trở lên trong tháng 4, bonus thêm 5% trên tổng hoa hồng được duyệt trong tháng 5
+
+📣 Chương trình 2: thưởng khi tăng trưởng về brand bonus trong tháng 5
+
+🔺 Với publisher đã có đơn thuộc brand bonus được duyệt trong tháng 4
+🔶 Nếu đơn thuộc brand bonus được duyệt tăng trưởng 1.5 lần so với tháng 4, bonus 3% trên tổng hoa hồng brand bonus được duyệt trong tháng 5
+🔶 Nếu đơn thuộc brand bonus được duyệt tăng 2 lần so với tháng 4, bonus 5% trên tổng hoa hồng brand bonus được duyệt trong tháng 5
+🔶 Nếu đơn thuộc brand bonus tăng từ 3 lần trở lên so với tháng 4, bonus 7% trên tổng hoa hồng brand bonus được duyệt trong tháng 5
+🔺 Với Publisher chưa có đơn thuộc brand bonus trong tháng 4
+🔶 Nếu publisher có từ 20 đơn được duyệt thuộc brand bonus trở lên, bonus thêm 10% trên tổng hoa hồng brand bonus được duyệt trong tháng 4`;
+
+
+var camp_recognition_conditions = `Từ 0:00  ngày 28/11/2020, chiến dịch sẽ thay đổi ghi nhận như sau:
+
+Áp dụng trên ứng dụng mua sắm trên điện thoại và webite của Shopee trên PC, đơn hàng đặt mua qua nền tảng web trên điện thoại vẫn sẽ được ghi nhận. 
+
+Khách hàng mua hàng và hoàn thành thanh toán, không đổi trả hàng (trong vòng 7 ngày). 
+
+Tỷ lệ lost đơn ngẫu nhiên ~20% giữa các thiết bị điện thoại, các thiết bị IOS có tỷ lệ lost đơn cao hơn`;
+
+
+var camp_cancellation_reason = `- Khách hàng không nhận hàng và thanh toán, khách hàng trả hàng
+
+- Trường hợp thiết bị của người dùng chưa có App, trong trường hợp người dùng click button hoặc popup hiển thị thay vì bấm về Web Browser để chuyển sang app store đơn hàng sẽ không được ghi nhận.
+
+- Các đơn hàng gian lận, các mặt hàng bị cấm quảng bá.`;
+
+var camp_cookie_policy = `Từ 0:00 ngày 28/11/2020 luật cookie sẽ như sau:
+
+- Mua hàng trên Mobile App
+
+ Giới hạn đơn hàng trong thời gian lưu cookie
+
+Cookie  bị ghi đè bởi các quảng cáo khác của Shopee.
+
+Cookie : 7 ngày
+
+- Mua hàng trên PC:
+
+Sau khi đơn hàng đầu tiên phát sinh, máy của người dùng sẽ không còn lưu cookie nữa
+
+Cookie bị ghi đè bởi các quảng cáo khác của Shopee
+
+Cookie 7 ngày
+
+Lưu ý: cookie không được ghi nhận nếu đối tác tạo link trên các trang như login, cart, payment  Do vậy đối tác không tạo link quảng bá cho những trang này để tránh không ghi nhận đơn hàng.
+
+Hiện tại  publishers sẽ thấy tỷ lệ huỷ do nguồn khác và tỷ lệ huỷ chung cho chiến dịch trên PC, lý do Hasoffer chưa loại bỏ các đơn hàng theo luật last click với channel khác trừ channel affiliate . (Chạy trên app không bị hiện tượng này nên tỷ lệ duyệt chung sẽ cao hơn PC)`;
+
+var camp_note = `Đối tác không Sử dụng các từ khóa chứa: Shopee, Shoppee, Shope, Shoppe chạy quảng cáo kể cả cụm từ longtail có tên thuong hiệu Shopee ( vd: Cửa hàng Shopee, Shopee mua bán trực tuyến...)
+ Danh sách các từ khóa không được chạy quảng cáo Google adwords: https://docs.google.com/spreadsheets/d/15aHb8HYnXK9Za6UgQHShGLMVi-eMjO2a/edit#gid=1481052916 
+Đối tác không sử dụng brand Shopee tạo group, fanpage, website… dưới bất kì hình thức nào để truyền thông
+Đối tác không được tự xây dựng shop bán hàng trên Shopee
+Đối tác không được tự xây dựng shop bán hàng trên Shopee và tự mua hàng để nhận hoa hồng.
+Đối tác không được sử dụng hoạt động đầu cơ mua đi bán lại các sản phẩm của Shopee
+Các Publisher chạy hình thức reivew site không được sử dụng các nội dung tiêu cực, so sánh tiêu cực các sản phẩm được bán trên các sàn TMĐT với nhau. Hành động đó vi phạm chính sách thương hiệu của các sản phẩm được bán trên sàn TMĐT. Publisher vi phạm sẽ bị loại khỏi chiến dịch.
+Từ ngày 27/08/2019 Publisher lưu ý thêm các quy định sau:
+
+Hình thức chạy chiến dịch bằng email marketing sẽ không được khuyến khích, trong trường hợp chạy email marketing, Publisher phải đảm bảo không sử dụng thương hiệu cũng như từ "Shopee" trong địa chỉ thư điện tử, trong URL, trong mã nguồn, và trong tiêu đề của thư điện tử để tránh gây hiểu lầm cho khách hàng đó là mail của Shopee gửi cho họ. Nội dung mail cũng phải đảm bảo không chứa nội dung cấm và gây ảnh hưởng đến thương hiệu Shopee
+Publisher không được thực hiện hành vi đặt hộ đơn hàng
+Publisher không được phép sử dụng thương hiệu cũng như từ "Shopee" trong địa chỉ thư điện tử, trong URL, trong mã nguồn và trong tiêu đề của thư điện tử.
+Publisher không tự ý sửa đổi các tài liệu quảng cáo hoặc sử dụng các hình ảnh thương hiệu của Shopee để quảng bá mà không phải được cung cấp từ ACCESSTRADE.
+
+
+Các quy định được cập nhật thêm từ ngày 1.12.2021: tại đây
+
+Nếu phát hiện hoặc nghi ngờ publisher vi phạm chính sách trên hoặc publisher gian lận trong việc thực hiện việc quảng bá, gian lận đơn hàng sẽ bị ngưng quyền chạy chiến dịch ngay lập tức và sẽ ngưng thanh toán tất cả đơn hàng đã phát sinh trước đó.
+Trường hợp vi phạm nghiêm trọng ảnh hưởng đến Shopee, publisher sẽ bị phạt 30% doanh thu 3 tháng gần nhất.
+
+LƯU Ý QUAN TRỌNG:
+
+- Từ 0:00 ngày 2/11/2020 chiến dịch Shopee sẽ chuyển qua sử dụng hệ thống mới của Shopee thay vì hệ thống Tracking Partnerize
+
+- Chiến dịch chạy trên hệ thống mới của Shopee sẽ lên đơn hàng sau 24h kể từ lúc đơn hàng phát sinh
+
+- Chiến dịch sẽ chỉ hiển thị những đơn hàng không hủy luôn trong 24h kể từ thời điểm đơn được đạt thành công, đơn hàng bị hủy trong 24h sẽ không hiển thị trên hệ thống
+
+- Trường hợp click link tracking sau đó mua ngay, thời gian mua hàng diễn ra quá nhanh shopee sẽ nhận diện là đơn spam và đơn sẽ không lên hệ thống.
+
++ Trong thời gian lưu Cookies, Shopee chỉ đang ghi nhận hoa hồng cho đơn hàng đầu tiên. Trước khi thực hiện mua đơn hàng tiếp theo, bạn cần nhấp lại vào link affiliate để được ghi nhận hoa hồng cho đơn hàng này.
+
++ Sản phẩm user đã bỏ vào giỏ hàng trước đó, rồi mới nhấp vào link affliate và thực hiện thanh toán thường sẽ có tỉ lệ lost cao.`;
+
