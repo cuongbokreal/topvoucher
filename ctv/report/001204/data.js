474ba9b87173f09e11c2b359d9bcfb259aa99d8a
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-06-07 15:58:39 +0700

    Update data.js

--- ctv/report/001204/data.js
+++ ctv/report/001204/data.js
@@ -2,30 +2,6 @@ var pubName = 'Cuongbok';
 
 var dataDoanhThu = [
   {
-   "nhaQuangCao":"Shopee",
-   "chuyenDoiPhatSinh": 165 ,
-   "giaTriDonHang":  4486790,
-   "hoaHong": 286187 ,
-   "duocDuyet": 99419 ,
-   "daHuy": 27449 ,
-  },
-  {
-   "nhaQuangCao":"Lazada",
-   "chuyenDoiPhatSinh": 12 ,
-   "giaTriDonHang": 632021 ,
-   "hoaHong": 56213 ,
-   "duocDuyet": 22584 ,
-   "daHuy": 2365 ,
-  },
-  {
-   "nhaQuangCao":"Tiki",
-   "chuyenDoiPhatSinh": 5 ,
-   "giaTriDonHang": 1254863 ,
-   "hoaHong": 125036 ,
-   "duocDuyet": 87012 ,
-   "daHuy": 0 ,
-  },
-  {
    "nhaQuangCao":"Thưởng thêm (Brand Bonus)",
    "giaTriDonHang": 198246 ,
    "chuyenDoiPhatSinh": 26 ,
