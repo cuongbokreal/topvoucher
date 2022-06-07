6e83527ef22f5c5456929be029db0719707e47f8
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-06-07 15:52:48 +0700

    Update api.js

--- ctv/report/api.js
+++ ctv/report/api.js
@@ -32,14 +32,14 @@ function doanhthu(){
   document.getElementById("thanhtoan").className = "px-3 border-b-2 border-transparent text-gray-600 dark:text-gray-400 pb-1.5";
   document.getElementsByClassName("doanhthu")[0].style.display='block';
   document.getElementsByClassName("thanhtoan")[0].style.display='none';
-  innerChuyenDoiTotal2()
+  //innerChuyenDoiTotal2()
 }
 function thanhtoan(){
   document.getElementById("thanhtoan").className = "px-3 border-b-2 border-blue-500 text-blue-500 dark:text-white dark:border-white pb-1.5";
   document.getElementById("doanhthu").className = "px-3 border-b-2 border-transparent text-gray-600 dark:text-gray-400 pb-1.5";
   document.getElementsByClassName("doanhthu")[0].style.display='none';
   document.getElementsByClassName("thanhtoan")[0].style.display='block';
-  innerSoDuTotal2()
+  //innerSoDuTotal2()
 }
 
 var param_time_end = getUrlParameter('update_time_end');
