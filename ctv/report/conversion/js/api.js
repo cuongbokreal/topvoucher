2fd684e9ceaab43d1c36957bda5870e5672c3867
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-06-09 14:08:32 +0700

    Update api.js

--- ctv/report/conversion/js/api.js
+++ ctv/report/conversion/js/api.js
@@ -81,7 +81,7 @@ fetch(fetch_doanhthu_url, { headers })
                 data-sale_time="${data.data[key].sales_time}"
                 data-billing="${data.data[key].billing}"
                 data-pub_commission="${(data.data[key].pub_commission * tile).toLocaleString()}"
-                data-products="${data.data[key].products}"
+                data-products="${data.data[key].products.values()}"
                 title="${data.data[key].order_id}" href="#popup"
                         >${data.data[key].order_id}</a>
                       </span>
