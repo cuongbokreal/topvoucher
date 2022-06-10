5c5ee5efce182f52bc17b0f3322d71ab759f4fc5
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-06-10 13:01:13 +0700

    Update api.js

--- ctv/report/conversion/js/api.js
+++ ctv/report/conversion/js/api.js
@@ -26,8 +26,9 @@ document.getElementById('innerPubName').innerHTML = `id: <span class="text-red">
 
 var update_time_start = document.getElementById('update_time_start');
 var update_time_end = document.getElementById('update_time_end');
-
+update_time_start.setAttribute('max', `${thisYear}-${padLeadingZeros(thisMonth,2)}-${padLeadingZeros(thisDate-1,2)}`);
 update_time_end.setAttribute('max', `${thisYear}-${padLeadingZeros(thisMonth,2)}-${padLeadingZeros(thisDate,2)}`);
+
 var matchTime = /[0-9]{4}-[0-9]{2}-[0-9]{2}/g;
 
 if(typeof param_time_end == 'undefined' || param_time_end.length < 10 || param_time_end.length <= 1){
