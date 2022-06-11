2a37e7f24e03c67ebb1928f4e578272cd48dea69
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-06-11 21:38:58 +0700

    Update main.js

--- admin/tools/get-og-link/main.js
+++ admin/tools/get-og-link/main.js
@@ -19,79 +19,70 @@ function makeHttpObject() {
 var request = makeHttpObject();
 
 function getUrlOg(){
-  var linkSp = document.getElementById('linkSp').value;
-	linkSp = linkSp.match(/\bhttps?:\/\/\S+/g);
-	console.log(linkSp)
+	var linkSp = document.getElementById('linkSp').value;
 	var og_txt = document.getElementById('linkSp').value;
-  request.open("GET", linkSp, true);
-  request.send(null);
-  request.onreadystatechange = function() {
-    if (request.readyState == 4){
-      var linkSp = request.responseText;
-	    
-        if(linkSp.match(/shopee\.vn/g)){
+	linkSp = linkSp.match(/\bhttps?:\/\/\S+/g);
+	if(linkSp.length >= 1){
+		for(let i=0; i<linkSp.length; i++){
+			console.log(linkSp);
+			request.open("GET", linkSp[i], true);
+			request.send(null);
+			request.onreadystatechange = function() {
+				if (request.readyState == 4){
 
-            if(linkSp.match(/promotionId/g) || linkSp.match(/signature/g) || linkSp.match(/evcode/g)){
-              var evcode = linkSp.match(/evcode=?([a-zA-Z0-9]+)/g)[0];
-              var promotionId = linkSp.match(/&promotionId=?([0-9]+)/g)[0];
-              var signature = linkSp.match(/&signature=?([a-zA-Z0-9]+)/g)[0];
-              finalLink = encodeURIComponent(`https://shopee.vn/search?${evcode}${promotionId}${signature}`);
-            }else 
-            if(linkSp.match(/i\..+\?/g)){
-              var idSp = linkSp.match(/i\..+\?/g)[0];
-              finalLink = `https://shopee.vn/TrumGiamGia.Tk-${idSp}`;
-              finalLink = encodeURIComponent(finalLink.replaceAll("?",""));
-            }else 
-            if(linkSp.match(/https:\/\/shopee.vn\/.+\?/)){
-              finalLink = linkSp.match(/https:\/\/shopee.vn\/.+\?/);
-              finalLink = encodeURIComponent(finalLink[0].replaceAll("?",""));
-            }else{
-              finalLink = linkSp.match(/http.+/g);
-              finalLink = encodeURIComponent(finalLink[0].replaceAll(/\?.+/g,""));
-            }
-            kq = `${deeplink}/${data_ad[0].camp_id}?url=${finalLink}${source}`;
-          }else
+					if(linkSp.match(/shopee\.vn/g)){
 
-          if(linkSp.match(/lazada\.vn/g)){
-            if(linkSp.match(/c\.lazada\.vn/g)){
-              finalLink = linkSp.match(/url=\S+/g)[1].replaceAll('url=','');
-              console.log(linkSp.match(/url=\S+/g))
-            }else
-            if(linkSp.match(/https:\/\/(pages|www)\.lazada\.vn.+?laz_trackid/g)){
-              finalLink = linkSp.match(/https:\/\/(pages|www)\.lazada\.vn.+?laz_trackid/g);
-              //add '?referer=at-kol'
-              finalLink = encodeURIComponent(addRefKolLazada(finalLink[0].replace("laz_trackid"," ").replace(/(\?\s|&\s)/g,"")));
-            }else{
-              //add '?referer=at-kol'
-              finalLink = encodeURIComponent(addRefKolLazada(linkSp));
-            }
-            kq = `${deeplink}/${data_ad[1].camp_id}?url=${finalLink}${source}`;
-          }else
-          if(linkSp.match(/tiki\.vn/g)){
-            if(linkSp.match(/https:\/\/tiki.vn\/.+\?/)){
-                finalLink = linkSp.match(/https:\/\/tiki.vn\/.+\?/);
-                finalLink = encodeURIComponent(finalLink[0].replaceAll("?",""));
-              }else{
-                finalLink = linkSp.match(/http.+/g);
-                finalLink = encodeURIComponent(finalLink[0].replaceAll(/\?.+/g,""));
-              }
-            //finalLink = linkSp.match(/http.+\?/g);
-            //finalLink = encodeURIComponent(finalLink[0].replaceAll("?",""));
-            kq = `${deeplink}/${data_ad[2].camp_id}?url=${finalLink}${source}`;
-          }
-        kqDeeplink.value = kq;
-        infor_camp.innerHTML = data_infor_camp;
-        console.log(finalLink)
-        if(createShortenLink.checked == true){
-          get_short_url(bitly_token, kq); //get shortlink nếu có
-        }else{kqShortlink.value = '';} //check có tạo short thì tạo, không thì kqShortLink value = '';
+					    if(linkSp.match(/promotionId/g) || linkSp.match(/signature/g) || linkSp.match(/evcode/g)){
+					      var evcode = linkSp.match(/evcode=?([a-zA-Z0-9]+)/g)[0];
+					      var promotionId = linkSp.match(/&promotionId=?([0-9]+)/g)[0];
+					      var signature = linkSp.match(/&signature=?([a-zA-Z0-9]+)/g)[0];
+					      finalLink = encodeURIComponent(`https://shopee.vn/search?${evcode}${promotionId}${signature}`);
+					    }else 
+					    if(linkSp.match(/i\..+\?/g)){
+					      var idSp = linkSp.match(/i\..+\?/g)[0];
+					      finalLink = `https://shopee.vn/TrumGiamGia.Tk-${idSp}`;
+					      finalLink = encodeURIComponent(finalLink.replaceAll("?",""));
+					    }else 
+					    if(linkSp.match(/https:\/\/shopee.vn\/.+\?/)){
+					      finalLink = linkSp.match(/https:\/\/shopee.vn\/.+\?/);
+					      finalLink = encodeURIComponent(finalLink[0].replaceAll("?",""));
+					    }else{
+					      finalLink = linkSp.match(/http.+/g);
+					      finalLink = encodeURIComponent(finalLink[0].replaceAll(/\?.+/g,""));
+					    }
+					  }else
 
-                  if(thbao.style.display == 'none'){toast({title: 'Thành công', message: `Đã tạo link`, type: "success", duration: time_thbao});}
-                  else {toast({title: 'Lỗi', message: `Đã có lỗi xảy ra`, type: "error", duration: time_thbao});}
-
-        return kq
-    }//end request done
-  }
+					  if(linkSp.match(/lazada\.vn/g)){
+					    if(linkSp.match(/c\.lazada\.vn/g)){
+					      finalLink = linkSp.match(/url=\S+/g)[1].replaceAll('url=','');
+					      console.log(linkSp.match(/url=\S+/g))
+					    }else
+					    if(linkSp.match(/https:\/\/(pages|www)\.lazada\.vn.+?laz_trackid/g)){
+					      finalLink = linkSp.match(/https:\/\/(pages|www)\.lazada\.vn.+?laz_trackid/g);
+					      //add '?referer=at-kol'
+					      finalLink = encodeURIComponent(addRefKolLazada(finalLink[0].replace("laz_trackid"," ").replace(/(\?\s|&\s)/g,"")));
+					    }else{
+					      //add '?referer=at-kol'
+					      finalLink = encodeURIComponent(addRefKolLazada(linkSp));
+					    }
+					  }else
+					  if(linkSp.match(/tiki\.vn/g)){
+					    if(linkSp.match(/https:\/\/tiki.vn\/.+\?/)){
+						finalLink = linkSp.match(/https:\/\/tiki.vn\/.+\?/);
+						finalLink = encodeURIComponent(finalLink[0].replaceAll("?",""));
+					      }else{
+						finalLink = linkSp.match(/http.+/g);
+						finalLink = encodeURIComponent(finalLink[0].replaceAll(/\?.+/g,""));
+					      }
+					    //finalLink = linkSp.match(/http.+\?/g);
+					    //finalLink = encodeURIComponent(finalLink[0].replaceAll("?",""));
+					  }
+				}//end request done
+			}
+			og_txt = og_txt.replaceAll(linkSp[i], decodeURIComponent(finalLink))
+		}
+		document.getElementById('linkSp').value = og_txt;
+	}
 }
 
 
