43dd28e965fad5c4cdcc45dc6beeb0aa860098e8
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-06-12 23:10:15 +0700

    Create data.js

---
+++ ctv/report/021206/data.js
@@ -0,0 +1,19 @@
+var pubName = 'Le Hong Quan';
+
+var dataDoanhThu = [
+  {
+   "nhaQuangCao":"Giới thiệu CTV",
+   "chuyenDoiPhatSinh": 0 ,
+   "giaTriDonHang": 0 ,
+   "hoaHong": 0 ,
+   "duocDuyet": 0 ,
+   "daHuy": 0 ,
+  },
+];
+
+var dataThanhToan = [
+  {"thangDoiSoat":"06-2022",
+   "hoaHongDuocDuyet": 0 ,
+   "daThanhToan": 0 ,
+  },
+]
