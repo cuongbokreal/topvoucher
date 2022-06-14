da37018afcda9e8310de436e0349fada1de450f8
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-06-14 10:12:30 +0700

    Create data.js

---
+++ ctv/tools/tong-hop/data.js
@@ -0,0 +1,90 @@
+⏰SHOPEE, LAZADA TỪ 9H - 11H NGÀY 14.6:
+➖➖➖➖SHOPEE➖➖➖➖
+🚛MIỄN PHÍ VẬN CHUYỂN: canh back lúc 9H
+• 9H: Lưu mã Freeship 0Đ giảm 10K, 70K + mã Freeship Xtra tại: https://shopee.vn/universal-link/m/ma-giam-gia-14-06
+.
+🔥MÃ TOÀN SÀN - NHẬP TAY:  canh back lúc 9H
+• 8H57-9H03: HUNT156KPH giảm 15% tối đa 156K đơn từ 156K. List sản phẩm áp mã: https://shopee.vn/search?evcode=SFVOVDE1NktQSA&promotionId=377743625060352&signature=7e2e943bb80e5719948786c59106334601e17585be3cde81b6d335e0a896891b (back)
+• 8H30-9H: SHOPEE156 giảm 10% tối đa 50K đơn từ 0Đ (thường back lúc 8H30)
+• 146HOAN10K0PH hoặc 146HOAN10K1PH hoặc 146HOAN10K2PH hoàn 15% tối đa 10K đơn từ 50K
+• 96GIAM40K0PH hoặc 96GIAM40K1PH hoặc 96GIAM40K2PH giảm 5% tối đa 40K đơn từ 500K
+• 96MALL50K0PH hoặc 96MALL50K1PH hoặc 96MALL50K2PH giảm 6% tối đa 50K đơn từ 250K(Mall)
+• SHOPEEGIAM50 giảm 50% tối đa 100K đơn từ 0Đ - người mới
+.
+🔥MÃ ÁP SẢN PHẨM CÓ MÁC “Hoàn Xu Xtra” hoặc “Freeship Xtra”: canh back lúc 9H
+• Mã hoàn 6% tối đa 200K đơn từ 400K(Hoàn Xu Xtra)
+• Mã giảm 5% tối đa 100K đơn từ 500K(Freeship Xtra)
+• Lưu sớm 2 mã hoàn 200K, 300K cho sale 15.6
+=> Lưu mã tại: https://shopee.vn/universal-link/m/shopee-cashback-1406
+.
+🔥SỨC KHỎE SẮC ĐẸP: https://shopee.vn/universal-link/m/sieu-sale-my-pham-fmcg1406
+• 9H: Săn mã shop giảm 50% tối đa 50K đơn từ 0Đ
+.
+🔥Kính Mắt Lily: https://shopee.vn/universal-link/m/lily-1406
+• 9H: Săn mã shop giảm 200K đơn từ 0Đ
+• 10H:  LILYLOVE2 (mã shop) giảm còn 0Đ cho Gọng kính: https://shopee.vn/universal-link/product/230940122/5072163564
+• 11H: LILYGIFT2 (mã shop) giảm còn 14K cho Gọng kính: https://shopee.vn/universal-link/product/230940122/5072163564
+.
+🔥SHOPEEFOOD: https://shopee.vn/universal-link/m/dat-do-an-online-shopeefood
+• 11H: Săn mã giảm 18K phí Ship cho đơn từ 0Đ
+.
+🔥MÃ ÁP MỘT SỐ SẢN PHẨM: dùng luôn
+• AFF6FSW giảm 20K đơn từ 99K. List sản phẩm áp mã: https://shopee.vn/search?evcode=QUZGNkZTVw&promotionId=380853188706304&signature=2570f6ce124cd2fe5195287b2efcdc11eec1be1a12e90fbc5c77fe37c9e7362c
+• SPFDEAL1006 hoàn 50K đơn từ 200K. List sản phẩm áp mã: https://shopee.vn/search?evcode=U1BGREVBTDEwMDY&promotionId=375847454572544&signature=ca0cd400ac9b4dcd0e71dc53643d073739c6bf1e1d2100be557e81f0adf81b73
+• I091515K giảm 15K đơn từ 0Đ. List sản phẩm áp mã: https://shopee.vn/search?evcode=STA5MTUxNUs&promotionId=372857914064896&signature=df8f8319ae81bab01a74b8db4d9353f3efaafe24d984fbf77d84febaa09842df
+• SPFDEAL0906 hoàn 25K đơn từ 60K. List sản phẩm áp mã: https://shopee.vn/search?evcode=U1BGREVBTDA5MDY&promotionId=375847447298048&signature=ba0c85c9e736e74268ea0cc38fb77d26dcb1c4bad82e92a4df586508cf8431c6
+• AFF66CPNW hoàn 20K đơn từ 50K. List sản phẩm áp mã: https://shopee.vn/search?evcode=QUZGNjZDUE5X&promotionId=374940771827712&signature=748fde97f20064263f80b476b162c02fbabb8e78247b4632c8893cf777ab125b
+@All 
+🔥SHOPEEPAY: https://shopee.vn/universal-link/m/shopeepay 
+• 9H: SPPD06221450 giảm 15K đơn từ 30K(back)
+• Lưu sớm mã giảm 50K đơn từ 100K ShopeePay cho 16.6
+.
+🔥MÃ ÁP MỘT SỐ SẢN PHẨM: canh back lúc 9H
+• I091520K giảm 20K đơn từ 0Đ. List sản phẩm áp mã: https://shopee.vn/search?evcode=STA5MTUyMEs&promotionId=372857914212352&signature=b951e0f72678a684ea49daf06713a31ca22a3aa5c5d46290268e2302e10cebfe
+• I091515K giảm 15K đơn từ 0Đ. List sản phẩm áp mã: https://shopee.vn/search?evcode=STA5MTUxNUs&promotionId=372857914064896&signature=df8f8319ae81bab01a74b8db4d9353f3efaafe24d984fbf77d84febaa09842df
+• SKAMNLT06 giảm 20K đơn từ 0Đ. List sản phẩm áp mã: https://shopee.vn/search?evcode=U0tBTU5MVDA2&promotionId=373535177801728&signature=847312e8c0b49b7c7f559f3c66d6a9db4e80eee4117509713d49ba77e84d107a
+.
+🔥TIÊU DÙNG: Sức khoẻ sắc đẹp(mỹ phẩm,..), bách hoá online, mẹ bé, giặt giũ, thú cưng
+• 12H: Mã giảm 50% tối đa 100K đơn từ 0Đ(đúng giờ load link, mã sẽ hiện ở dưới mã Freeship giảm 70K)
+=> Lưu mã tại: https://shopee.vn/universal-link/m/sieu-sale-nganh-hang
+.
+🔥MÃ QUỐC TẾ: đúng giờ mới hiện mã (lưu dùng nhanh)
+• 12H: Mã giảm 50% tối đa 20K đơn từ 0Đ(12H load link chọn mục "GIỜ VÀNG SĂN SALE" để tới thẳng vị trí lên mã)
+=> Lưu mã tại: https://shopee.vn/universal-link/m/sieu-sale-hang-quoc-te
+.
+🔥SIÊU SALE VOUCHER: 0H ngày 15.6 dùng
+• Lưu sớm mã giảm 156K/1t5, 15K/50K, 150K/3tr, 10% cho toàn sàn
+• Lưu sớm mã giảm 50K, 100K, 150K, 200K đơn từ 0Đ cho các thương hiệu
+=> Lưu sớm các mã tại: https://shopee.vn/universal-link/m/sieu-sale-voucher-6
+.
+🔥CHỜ ĐÓN SIÊU SALE 15.6:
+• Lưu sớm mã Freeship 0Đ giảm 15K + mã toàn sàn 70K + ShopeeMall 50K
+=> Lưu sớm mã tại: https://shopee.vn/universal-link/m/don-cho-sieu-sale-09-06
+.
+🔥Mở hộp quà: https://shopee.vn/universal-link/m/sieu-sale-nganh-hang-14-06 (lượt mới 15H)
+.
+🔥Trả lời câu hỏi nhận xu hoặc voucher: 
+=> Link 1: https://shopee.vn/universal-link/m/sieu-sale-hang-quoc-te-14-6 đáp án: B A C D D
+=> Link 2: https://shopee.vn/universal-link/m/sieu-sale-my-pham-fmcg1406 đán án: D (mở thêm lúc 12H, 18H)
+.
+.
+➖➖➖➖LAZADA➖➖➖➖
+🔥9H, 12H: MÃ NGâN HÀNG: 
+=> Lưu mã tại: https://pages.lazada.vn/wow/gcp/lazada/channel/vn/partnership/uu-dai-doi-tac?referer=at-kol
+.
+🔥MOMO: canh back 9H hàng ngày - có thể lên sớm hoặc muộn vài phút
+=> Lưu mã tại: https://pages.lazada.vn/wow/gcp/lazada/channel/vn/thanh-toan/khuyen-mai-momo?referer=at-kol
+.
+🔥ZALOPAY: canh back 9H, 12H hàng ngày - có thể lên sớm hoặc muộn vài phút
+=> Lưu mã tại:  https://pages.lazada.vn/wow/gcp/lazada/channel/vn/thanh-toan/khongtienmat?referer=at-kol
+.
+🔥VOUCHER TÍCH LŨY GIẢM TỚI 400K: dùng từ ngày 15.6-19.6
+• Lưu 30K tích luỹ tại: https://pages.lazada.vn/wow/gcp/route/lazada/vn/upr_1000345_lazada/channel/vn/upr-router/vn?hybrid=1&data_prefetch=true&prefetch_replace=1&at_iframe=1&wh_pid=/lazada/megascenario/vn/sale-giua-thang-06-2022/voucher-tich-luy-400k&referer=at-kol
+• Lưu thêm 30K tích luỹ dưới sản phẩm tại: https://www.lazada.vn/products/co-bon-cau-toilet-co-ve-sinh-cao-cap-parroti-silicon-sl01-co-coc-dung-choi-cay-cap-toc-rung-tien-dung-i896122762-s2591888991.html?referer=at-kol
+.
+🔥FREESHIP:
+• Mã Freeship 15K/49K, 25K/99K, 50K/300K
+=> Lưu mã tại: https://pages.lazada.vn/wow/gcp/lazada/channel/vn/voucher/claimvoucher?referer=at-kol
+.
+🔥LAZMALL GIẢM 60K ĐƠN TỪ 499K:
+=> Lưu mã tại: https://pages.lazada.vn/wow/gcp/lazada/channel/vn/khuyen-mai/LazMall_Hot_Deals?referer=at-kol
