d99fb1ac3e453d66f4d1306375f4fadde981ff3a
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-06-24 20:47:29 +0700

    Update data.js

--- ctv/tools/tong-hop/data.js
+++ ctv/tools/tong-hop/data.js
@@ -1,59 +1,333 @@
-🎁 BẬT BÍ TRƯỚC MÃ SHOPEE ĐÊM NAY
-------------------------------------
+⚡TIẾT LỘ SỚM 95% MÃ SHOPEE NGÀY 25.6:
+
+🔥MÃ TOÀN SÀN - NHẬP TAY - KHÔNG LỌC TÀI KHOẢN:
+
+• 0H: 256HUNT1000K0H hoàn 15% tối đa 1triệu đơn từ 2triệu
+
+• 0H: 256HUNT256K0H hoàn 25% tối đa 255K đơn từ 255K
+
+• 0H: 256GIAM9K giảm 9K đơn từ 0Đ
+
+• 9H: 256HUNT256K9H hoàn 25% tối đa 255K đơn từ 255K
+
+• 12H: 256HUNT256K12H hoàn 25% tối đa 255K đơn từ 255K
+
+• 15H: 256HUNT256K15H hoàn 25% tối đa 255K đơn từ 255K
+
+• 16H: DROP30K16H hoàn 30K đơn từ 0Đ
+
+• 18H: 256HUNT256K18H hoàn 25% tối đa 255K đơn từ 255K
+
+• 21H: 256HUNT256K21H hoàn 25% tối đa 255K đơn từ 255K
+
+=> Áp chung list sản phẩm tại: https://shopee.vn/search?evcode=RFJPUDMwSzE2SA&promotionId=385825779367936&signature=d2b6743c58f2383ab0fef33598d259528927854bc2a5559d25c9885059ec24a7
+
 .
-🔥 1. MÃ ĐỘC QUYỀN: Bật bí sớm tại Tele và Zalo
-•	Ae nên tải và đăng kí Tele để nhận được thông báo deal sớm nhất nhé
-•	Cập nhật thêm 30 link KOL 30K, 20K, 50K/0Đ sớm nhất tại Tele
+
+🔥SIÊU SALE VOUCHER: 1 số mã đã lưu sớm được + 1 số mã đúng giờ mới hiện
+
+• 0H, 9H, 12H: Mã giảm 256K đơn từ 2560K toàn sàn
+
+• 1H, 7H: Mã giảm 30K đơn từ 150K toàn sàn
+
+• 0H, 9H, 12H, 18H: Mã giảm 25K đơn tư 75K toàn sàn
+
+• 0H, 9H, 12H: Mã giảm 10% tối đa 50K đơn từ 300K toàn sàn
+
+• 0H: Mã giảm 150K đơn từ 3 triệu toàn sàn
+
+• 8H, 11H, 17H: Mã hoàn 20K đơn tư 50K toàn sàn
+
+• 0H: Mã giảm 300K, 100K, 70K,.. từ các thương hiệu
+
+=> Lưu mã tại: https://shopee.vn/universal-link/m/sieu-sale-voucher-6
+
 .
-🔥 2. MIỄN PHÍ VẬN CHUYỂN: https://shorten.asia/sVr9M1dq
-•	Reset ưu đãi tuần: : Lưu các mã MPVC 15K/0Đ hoặc 10K/0Đ và 50% max 15K/0Đ tại banner (thêm lượt 9h, 12h, 15h, 18h, 21h)
-•	Lưu thêm các mã MPVC khác: https://shorten.asia/94PmBH3Z
+
+🔥MÃ TOÀN SÀN - LƯU BANNER: đúng giờ load link mới hiện mã
+
+• Mã giảm 30K/99K, 30K/150K, 50K, 10K,..(cụ thể lịch lên mã  + chi tiết loại mã chính xác mình cập nhật sau ở bài tổng hợp mã tối nay)
+
+=> Lưu mã tại: https://shopee.vn/universal-link/m/kol-affiliate
+
 .
-🔥 3. MÃ NHẬP TAY HOT: https://shorten.asia/sVr9M1dq
-(Code hiện đang lỗi, nếu có thay đổi, tớ cập nhật sau)
-•	0h: 256HUNT1000K0H hoàn 15% max 1 triệu đơn từ 2 triệu toàn sàn. Áp list: https://shorten.asia/E2nqTzMs
-•	0h: 256HUNT256K0H hoàn 25% max 255K đơn từ 255K toàn sàn. Áp list: https://shorten.asia/a3uRASWu
-•	0h: 256GIAM9K giảm 9K đơn từ 0đ toàn sàn. Áp list: https://shorten.asia/csXws2sw
-•	9h: 256HUNT256K9H hoàn 25% max 255K đơn từ 255K toàn sàn. Áp list: https://shorten.asia/xm1M9n8t
-•	12h: 256HUNT256K12H hoàn 25% max 255K đơn từ 255K toàn sàn. Áp list: https://shorten.asia/7RvePDnM
-•	15h: 256HUNT256K15H hoàn 25% max 255K đơn từ 255K toàn sàn. Áp list: https://shorten.asia/A22vZnUz
-•	16h: DROP30K16H hoàn 30K đơn từ 0đ toàn sàn. Áp list: https://shorten.asia/5p6grh5t
-•	18h: 256HUNT256K18H hoàn 25% max 255K đơn từ 255K toàn sàn. Áp list: https://shorten.asia/p6RkRwHg
-•	21h: 256HUNT256K21H hoàn 25% max 255K đơn từ 255K toàn sàn. Áp list: https https://shorten.asia/3wtRA5wd
+
+🔥CHỜ ĐÓN SIÊU SALE: 0H ngày 25.6 dùng được
+
+• Mã hoàn 8% tối đa 70K đơn từ 500K(toàn sàn)
+
+• Mã giảm 15% tối đa 15K đơn từ 99K(toàn sàn)
+
+• Mã giảm 8% tối đa 50K đơn từ 250K(Mall)
+
+• Mã giảm 20% tối đa 10K đơn từ 20K(ShopeePay)
+
+• Mã giảm 10% tối đa 15K đơn từ 100K(ShopeePay)
+
+• Mã giảm 10K đơn từ 0Đ(ShopeePay)
+
+• Mã Freeship 0Đ giảm 15K
+
+=> Lưu sớm mã tại: https://shopee.vn/universal-link/m/don-cho-sieu-sale
+
 .
-📌 3.1 Mã nhập tay dễ săn : https://shorten.asia/ntywHBuD
-(Mã áp đa số sp: Thay đuôi 0 hoặc 1 hoặc 2 để lưu dùng)
-•	0h: 256GIAM70K0H0 | 1 | 2  giảm 7% max 70K đơn từ 500K toàn sàn. Áp list: https://shorten.asia/tu7hwv9s
-•	0h: 256HOAN15K0H0 | 1 | 2  hoàn 15% max 15K đơn từ 99K toàn sàn. Áp list: https://shorten.asia/bAE3qC8Y
-•	0h: 256MALL50K0H0 | 1 | 2  giảm 7% max 50K đơn từ 250K cho Shopee Mall. Áp list: https://shorten.asia/jfZ292ph
+
+🔥MÃ NHâN PHẨM - ÁP TOÀN SÀN SHOPEE: chỉ 1 số ít tài khoản có mã - 0H ngày 25.6 dùng
+
+• 0H: Mã giảm 15K đơn từ 0Đ(đã lưu được)
+
+• 0H: Mã giảm 666K đơn từ 0Đ(21H30, 22H30, 23H30 ngày 24.6 mở lưu sớm mã)
+
+=> Lưu sớm mã tại: https://shopee.vn/universal-link/m/tro-lai-san-deal-jan
+
+@All 
+
+🚛MIỄN PHÍ VẬN CHUYỂN:
+
+• 0H: Lưu 1 mã Freeship 0Đ giảm 15K + 1 mã Freeship Xtra + 2 mã Freeship 0Đ giảm 50% tối đa 15K + 1 mã Freeship 99K giảm 50% tối đa 30K tại: https://shopee.vn/universal-link/m/ma-giam-gia-25-06 (còn back vào 9H, 12H, 15H, 18H, 21H)
+
+• Tặng sẵn 1 số mã trong ví: https://shopee.vn/universal-link/user/voucher-wallet
+
+• Lưu sớm mã Freeship 0Đ giảm 15K cho 0H ngày 25.6: https://shopee.vn/universal-link/m/don-cho-sieu-sale
+
+• Quay mã Freeship 0Đ giảm 15K cho 0H ngày 25.6: https://shopee.vn/universal-link/pc_event
+
+• Đổi mã Freeship giảm 50% giảm tối đa 40K tại: https://shopee.vn/universal-link/m/uu-dai-shopee-rewards
+
 .
-📌 3.2 Mã áp cho một số sp dễ săn: Thêm lượt mới 9h, 12h, 15h, 18h, 21h
-•	SKAMNLT06 giảm 20k đơn từ 0đ cho list sản phẩm: https://shorten.asia/Vy5FcpT1
-•	AFF6GMVMN giảm 5% max 100k đơn từ 500k. Áp list: https://shorten.asia/Sb9ykA47
-•	0h: AFF256BNBG giảm 30k đơn từ 150k. Áp list: https://shorten.asia/TajAs97s
-•	0h: AFF256CB hoàn 25k đơn từ 66k. Áp list: https://shorten.asia/cBtQKqRw
-•	0h: AFF256DR giảm 20k đơn từ 99k. Áp list: https://shorten.asia/vdkp26Dy
-•	0h: AFF256CPNW hoàn 20k đơn từ 50k. Áp list: https://shorten.asia/sj7YD6jw
-•	0h: AFF256CPALL hoàn 20k đơn từ 50k. Áp list: https://shorten.asia/KzaJBusZ
-•	0h: IB2506 hoàn 20K đơn từ 0đ. Áp list:  https://shorten.asia/6C6JPRXq
-🔥Đừng quên vào 4 link này lưu hết các mã tại banner + dùng lúc 0H nhé cả nhà (23h30 sẽ hiện hết tất cả các mã)
 
-📌 4.1. Đón chờ Siêu Sale: https://shorten.asia/B3DdK3Jr
+🔥MÃ ÁP MỘT SỐ SẢN PHẨM:
 
-📌 4.2 Siêu Sale Voucher tháng 6: https://shorten.asia/YsFqmns4
+• 0H: AFF256CB hoàn 25K đơn từ 66K. List sản phẩm áp mã: https://shopee.vn/universal-link/m/uu-dai-shopee-rewards
 
-📌 4.3 Trở lại săn Deal: https://shorten.asia/kHFkH8ah
+• 0H: AFF256DR giảm 20K đơn từ 99K. List sản phẩm áp mã: https://shopee.vn/universal-link/m/uu-dai-shopee-rewards
 
-📌 4.4 Siêu Hội ShopeeLive : https://shorten.asia/sY3j4cma 
+• 0H: AFF256CPNW hoàn 20K đơn từ 50K. List sản phẩm áp mã: https://shopee.vn/search?evcode=QUZGMjU2Q1BOVw&promotionId=374940788654080&signature=d4f2e6350f463d21f142636895e2b83387a3f8b1a6234c6d193a6d43f752803f
 
-📌 4.5 Săn sale cùng KOL: https://shorten.asia/B15muhXw
-⭐️ 25/6 - Săn voucher cùng KOL: https://shorten.asia/B15muhXw
-► 0H; 12H; 15H; 18H: Lưu trên banner ở link này https://shorten.asia/B15muhXw + dùng ngay mã giảm 50K đơn từ 150K toàn sàn Shopee
+• 0H: AFF256CPALL hoàn 20K đơn từ 50K. List sản phẩm áp mã: https://shopee.vn/search?evcode=QUZGMjU2Q1BBTEw&promotionId=374940788654096&signature=0be8efb28a581750bcda377bd1d48bc3e8f0b9f42766dd654a0a1066272d75a6
 
-► 0H; 0H30; 9H; 12H: Lưu trên banner ở link này https://shorten.asia/B15muhXw + dùng ngay mã giảm 30K đơn từ 150K toàn sàn Shopee
+• AFF256BNBG giảm 30K đơn từ 150K. List sản phẩm áp mã: https://shopee.vn/search?evcode=QUZGMjU2Qk5CRw&promotionId=380851938672640&signature=6e72bc6795c6a134f338962e5460392c485cb6057cc86f66f97eb008b4494ff4
 
-► 12H; 15H; 18H; 21H: Lưu trên banner ở link này https://shorten.asia/B15muhXw + dùng ngay mã giảm 30K đơn từ 99K toàn sàn Shopee
+• I013006 giảm 15K đơn từ 0Đ. List sản phẩm áp mã: https://shopee.vn/search?evcode=STAxMzAwNg&promotionId=372857948749824&signature=a120603c99567db3c02eb66d8f51a1e968f3c04863bf369917dc6137d4270520
 
-► 9H, 12H; 15H: Lưu trên banner ở link này https://shorten.asia/B15muhXw + dùng ngay mã gtiảm 10K đơn từ 35K toàn sàn Shopee
+• SKAMNLT06 giảm 20K đơn từ 0Đ. List sản phẩm áp mã: https://shopee.vn/search?evcode=U0tBTU5MVDA2&promotionId=373535177801728&signature=847312e8c0b49b7c7f559f3c66d6a9db4e80eee4117509713d49ba77e84d107a
 
-► 8H, 10H; 11H; 12H; 13H; 14H; 16H; 17H; 19H: Lưu trên banner ở link này https://shorten.asia/B15muhXw + dùng ngay mã hoàn xu 20K đơn từ 50K toàn sàn Shopee
+• LIFEHL206 giảm 20K đơn từ 0Đ. List sản phẩm áp mã: https://shopee.vn/search?evcode=TElGRUhMMjA2&promotionId=373464285757440&signature=5f4e26ffa3dec1dbb8d97f2ddabc925311278c871fff436f734b248fcc90f733
+
+.
+
+🔥SHOPEEPAY: https://shopee.vn/universal-link/m/shopeepay
+
+• 0H: SPPJUN2510KMBS giảm 10K đơn từ 20K
+
+• 0H: SPPJUN250H1M giảm 3% tối đa  triệu đơn từ 2triệu
+
+• 0H: Mã giảm 10K/0Đ, giảm 20% lưu sớm tại: https://shopee.vn/universal-link/m/don-cho-sieu-sale
+
+.
+
+🔥SHOP LÊN SÀN MỚI: https://shopee.vn/universal-link/m/shop-moi-len-san-moi-2506
+
+• 0H: IB2506 hoàn 20K đơn từ 0Đ. List sản phẩm áp mã: https://shopee.vn/search?evcode=SUIyNTA2&promotionId=372857947504640&signature=ecb7bc59c05faabd1243ff250ebfed30a63a97e303f78e14413ba85f8bc03410
+
+• 0H: I2506 giảm 20K đơn từ 0Đ. List sản phẩm áp mã: https://shopee.vn/search?evcode=STI1MDY&promotionId=372857947570176&signature=300bb06069da099884282e84c083e8e8de141a1549d53e6b9cac808fabbc51e5
+
+• 0H: List deal đồng giá 1K: https://shopee.vn/universal-link/collections/835629
+
+• List deal đồng giá 9K: https://shopee.vn/universal-link/m/shop-moi-len-san-moi-2506
+
+.
+
+🔥SĂN SALE SHOP XỊN: https://shopee.vn/universal-link/m/san-sale-shop-xin
+
+• 0H: LFPJUN05 hoàn 30K đơn từ 50K. List sản phẩm áp mã: https://shopee.vn/search?evcode=TEZQSlVOMDU&promotionId=374855501561872&signature=2c54e64090d47db19827a47db68c87da7631d29daf02339216ff12e03ecd2a0c
+
+• 0H: LFPJUN06 giảm 10% tối đa 50K đơn từ 150K. List sản phẩm áp mã: https://shopee.vn/search?evcode=TEZQSlVOMDY&promotionId=374855509999616&signature=0915d8ac25b3e81e92d080ceea077afc028a4cccc924bbec192088b69852e807
+
+.
+
+🔥RẤT NHIỀU MÃ GIẢM 30K/99K, 30K/99K, 20K/50K, 50K,... SẼ ĐƯỢC CẬP NHẬT CHI TIẾT VÀO GẦN CÁC KHUNG GIỜ SĂN MÃ  TRONG NGÀY 25.6
+
+🔥THỜI TRANG: https://shopee.vn/universal-link/m/sale-thoi-trang-2506
+
+• 0H: 256FASHIONSALE giảm 15K đơn từ 150K. List sản phẩm áp mã: https://shopee.vn/search?evcode=MjU2RkFTSElPTlNBTEU&promotionId=387350643752960&signature=b4646f6802babee6dc6a1688b2d6e9d4d0cb62643a896e513983257f83c63e18
+
+.
+
+📌Săn deal đồng giá 1K các khung giờ 0H, 9H, 12H, 15H, 18H
+
+• 0H: List deal đồng giá 1K: https://shopee.vn/universal-link/collections/867591
+
+.
+
+📌Săn mã shop giảm 222K các khung giờ 0H, 9H, 12H, 15H, 18H, 21H
+
+• 0H: CHOOBE252 (mã shop) giảm 220K đơn từ 600K cho sp Choobe Official: https://shopee.vn/search?evcode=Q0hPT0JFMjUy&promotionId=384955202322432&signature=f6b3cec290072dc9542e639af8d76e9ef85eb8b324489ef094a88e2a360de1f8
+
+• 0H: THOIN7220 (mã shop) giảm 220K đơn từ 0Đ cho sp N7 Official: https://shopee.vn/search?evcode=VEhPSU43MjIw&promotionId=383533566869504&signature=ed3923ad449321e27ff27db621fa0b630c77bc49c6bc8d24ac796bf2d1394103
+
+.
+
+📌Xưởng Thời Trang:
+
+• 0H: Loạt mã shop giảm 50K/0Đ, 100K/0Đ, 5K/0Đ. Lưu sớm mã tại: https://shopee.vn/universal-link/m/voucher-shop-xuong-2506
+
+• Săn đồng giá 1K, x9K
+
+• 0H: List deal đồng giá 1K: https://shopee.vn/universal-link/collections/980865
+
+.
+
+📌Voucher Thời Trang: https://shopee.vn/universal-link/m/voucher-thoi-trang ?
+
+• Voucher ưu đãi từ các thương hiệu.
+
+• Voucher từ các shop bán.
+
+.
+
+🔥CÔNG NGHỆ: https://shopee.vn/universal-link/m/shopee-tech-zone
+
+• 0H: 256ELSALE giảm 7% tối đa 500K đơn từ 300K. List sản phẩm áp mã: https://shopee.vn/search?evcode=MjU2RUxTQUxF&promotionId=387350610264064&signature=989b02f9ce3a7a3abd23a7e7055551d472289f15aafe50d6aeb9170a8d688fa4
+
+• Săn deal hot công nghệ các khung giờ.
+
+.
+
+📌Công Nghệ Mall: https://shopee.vn/universal-link/m/thuong-hieu-dien-tu
+
+• ELMALL500 giảm 10% tối đa 500K đơn từ 500K
+
+• ELMALL1TR5 giảm 8% tối đa 1.5m đơn từ 5m
+
+• Săn deal hot công nghệ các khung giờ.
+
+.
+
+🔥ĐỜI SỐNG: https://shopee.vn/universal-link/m/sieu-hoi-song-chat-250622
+
+• 0H: 256LIFESALE giảm 8% tối đa 100K đơn từ 99K. List sản phẩm áp mã: https://shopee.vn/search?evcode=MjU2TElGRVNBTEU&promotionId=387350626746368&signature=1d5a67dd22a04f03edab2093a146ba1f8a0042ac9ffb50267615a472824d9bb6
+
+• Quà tặng 0Đ từ các shop bán, săn deal đồng giá x9K
+
+• 0H: List deal đồng giá 9K: https://shopee.vn/universal-link/collections/800291
+
+.
+
+📌Thương Hiệu Đời Sống: https://shopee.vn/universal-link/m/thuong-hieu-doi-song
+
+• LIFEMALLCP06 giảm 12% tối đa 80K đơn từ 250K
+
+• LIFEMALL06 giảm 10% tối đa 50K đơn từ 99K
+
+• LIFEHL06 giảm 10% tối đa 80K đơn từ 250K
+
+• LIFEBOOK06 giảm 10% tối đa 50K đơn từ 99K
+
+• Voucher ưu đãi từ shop bán, deal x9k
+
+.
+
+📌Book Club: https://shopee.vn/universal-link/m/book-club
+
+• LIFESBC06 giảm 10K đơn từ 50K
+
+• LIFESBCWG06 giảm 10% tối đa 30K đơn từ 99K
+
+• Voucher ưu đãi từ các thương hiệu, shop
+
+🔥TIÊU DÙNG: https://shopee.vn/universal-link/m/sieu-sale-fmcg-250622
+
+• 0H: 256FMCGSALE giảm 7% tối đa 100K đơn từ 500K. List sản phẩm áp mã: https://shopee.vn/search?evcode=MjU2Rk1DR1NBTEU&promotionId=387350593290240&signature=02d1410784fa35bdf9a44e5263ce3719679b282e98f4c52bef2f503f8b9c42e7
+
+• Săn deal 1K, 9K, 25K các khung giờ  0H, 9H, 12H, 15H, 18H, 21H:
+
+• 0H: List deal đồng giá 1K: https://shopee.vn/universal-link/collections/1004223
+
+• 0H: List deal đồng giá 9K: https://shopee.vn/universal-link/collections/1005327
+
+• 0H: List deal đồng giá 25K: https://shopee.vn/universal-link/collections/1005341
+
+• Săn voucher 50% các khung giờ:
+
+• 0H: 256K991 (mã shop) hoàn 50% tối đa 99K đơn từ 0Đ. List sản phẩm áp mã: https://shopee.vn/search?evcode=MjU2Szk5MQ&promotionId=388756306018304&signature=b110df49433f214bb1c8fa67bc438febd1a76cf1d43ec6a20f5baa74f80653b3
+
+• 0H: 256K992 (mã shop) hoàn 50% tối đa 99K đơn từ 0Đ. List sản phẩm áp mã: https://shopee.vn/search?evcode=MjU2Szk5Mg&promotionId=388756305821696&signature=e3ffdec80b9a479962b1ebc45840826ed74efeb75b6e1f52610adb47ca69b246
+
+• 0H: 256K993 (mã shop) hoàn 50% tối đa 99K đơn từ 0Đ. List sản phẩm áp mã: https://shopee.vn/search?evcode=MjU2Szk5Mw&promotionId=388756306034688&signature=bcb828bfc6642d9eb1f13b1669c6ae91cad7324b2320f3b7d5efc7238406f4d5
+
+• 0H: 256K994 (mã shop) hoàn 50% tối đa 99K đơn từ 0Đ. List sản phẩm áp mã: https://shopee.vn/search?evcode=MjU2Szk5NA&promotionId=388756307083264&signature=dbe77fe942ab994be55b210f02f0ed977c6176f26c96e8a692306d8300680baa
+
+• 0H: 256K995 (mã shop) hoàn 50% tối đa 99K đơn từ 0Đ. List sản phẩm áp mã: https://shopee.vn/search?evcode=MjU2Szk5NQ&promotionId=388756305952768&signature=38c790a0972e70bb90bfcde709e025504a97946552482456ddf65a15da9800ab
+
+• 0H: 256K996 (mã shop) hoàn 50% tối đa 99K đơn từ 0Đ. List sản phẩm áp mã: https://shopee.vn/search?evcode=MjU2Szk5Ng&promotionId=388756315455488&signature=8ac2acf196b68685c8e63d0aec1c8d1ca3cc483554e08cebba5a542aa4411785
+
+.
+
+📌Sức Khỏe Sắc Đẹp: https://shopee.vn/universal-link/m/sieu-sale-my-pham-fmcg-2506
+
+• 0H: List deal Mỹ Phẩm Hàn đồng giá 59K: https://shopee.vn/universal-link/collections/1012255
+
+• 12H, 21H: Làm mới list deal Mỹ Phẩm Hàn đồng giá 59K chủ động load banner: https://shopee.vn/universal-link/m/sieu-sale-my-pham-fmcg-2506
+
+• Voucher ưu đãi từ các shop bán.
+
+• Mua 1 tặng 1, giảm 50%, quà tặng 0Đ
+
+.
+
+📌Tiêu Dùng Mall: https://shopee.vn/universal-link/m/shopeemart
+
+• FMCGMALL giảm 8% tối đa 80K đơn từ 250K
+
+• FMCG100K giảm 100K đơn từ 1tr
+
+• FMCGHOT giảm 10% tối đa 80K đơn từ 400K
+
+• Lưu các mã freeship Shopee Mart
+
+.
+
+📌Beauty Club: https://shopee.vn/universal-link/m/beauty-club
+
+• COSBEAUTY25K6 hoàn 25k đơn từ 250K
+
+• COSLUX25 giảm 150K đơn từ 1.5m
+
+• Deal đồng giá 1K, x9K, quà tặng 0Đ
+
+.
+
+📌Mẹ bé – Mum Club: https://shopee.vn/universal-link/m/mum-club
+
+• FMCGMEXMKB6 hoàn 10% tối đa 60K đơn từ 599K
+
+• FMCGMEXTK6 hoàn 15% tối đa 30K đơn từ 99K
+
+.
+
+🔥HÀNG QUỐC TẾ: https://shopee.vn/universal-link/m/sandealquocte-2506
+
+• 0H: 256CBSALE giảm 10% tối đa 100K đơn từ 150K. List sản phẩm áp mã: https://shopee.vn/search?evcode=MjU2Q0JTQUxF&promotionId=387350626992128&signature=b64eb2ee9133cb533f7abe87e658e35eac492828dc1a920fc9aeebfec8b374fe
+
+• Săn deal x9K
+
+.
+
+🔥SHOP XU HƯỚNG: https://shopee.vn/universal-link/m/shop-xu-huong2506
+
+• BMLTA50 giảm 10% tối đa 50K đơn từ 99K
+
+• BMLT300 giảm 10% tối đa 300K đơn từ 499K
+
+• BMINC50 giảm 10% tối đa 50K đơn từ 99K
+
+• INCU300 giảm 10% tối đa 300K đơn từ 499k
+
+• Săn deal đồng giá 1K các khung giờ 0H, 12H, 18H:
+
+• 0H: List deal đồng giá 1K: https://shopee.vn/universal-link/collections/910303
+
+• 0H: List deal đồng giá 9K: https://shopee.vn/universal-link/collections/910302
+
+• Săn voucher 50K, 100K các khung giờ. Lưu sớm mã 0H tại banner: https://shopee.vn/universal-link/m/shop-xu-huong2506
