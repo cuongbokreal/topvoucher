1f08f4979da568dbb8d4cceaea48c1fb232a22c1
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-07-09 11:07:52 +0700

    Update main.js

--- ctv/tools/replace/shortlink/main.js
+++ ctv/tools/replace/shortlink/main.js
@@ -4,6 +4,14 @@ var thbao = document.getElementById('thbao');
 var btnDeplink = document.getElementById('btnDeplink');
 var kqDeeplink = document.getElementById('kqDeeplink');
 var kqShortLink = document.getElementById('kqShortLink');
+var bitly_token = [
+	"31ae1cab212d8603378badec7a05ad2936a30c6b", //tet
+	"b39094e48700c125f4b256c45f668a1986b12be3", //0
+	"46dcf76dc84d19bd90b7ce6b2b4c353a0254d82c", //1
+	"308c4e020fee4205fcc103007c63a8fa632d197f", //2
+	"e4245081f447e466106995c0721e8835e5145f21", //3
+	"9d4dc5b797e4b3c7805b4e118d972e98c66b82e4", //4
+]
 
 var kq = '';
 var finalLink = '';
@@ -123,16 +131,6 @@ async function getUrlOg(){
 	}
 }
 
-
-
-var bitly_token = [
-	"31ae1cab212d8603378badec7a05ad2936a30c6b", //tet
-	"b39094e48700c125f4b256c45f668a1986b12be3", //0
-	"46dcf76dc84d19bd90b7ce6b2b4c353a0254d82c", //1
-	"308c4e020fee4205fcc103007c63a8fa632d197f", //2
-	"e4245081f447e466106995c0721e8835e5145f21", //3
-	"9d4dc5b797e4b3c7805b4e118d972e98c66b82e4", //4
-]
 async function get_short_url(longUrl){
 	for(let i=0; i<bitly_token.length; i++){
 	  fetch(`https://api-ssl.bitly.com/v3/shorten?access_token=${bitly_token[i]}&longUrl=${encodeURIComponent(longUrl)}&format=json`)
