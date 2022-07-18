8ebc26e9cdff45c570be2168020a028321b7109e
Cuongbok <92067448+cuongbokreal@users.noreply.github.com>
2022-07-18 16:28:51 +0700

    Create config_firebase.js

---
+++ firebase/config_firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+      apiKey: "AIzaSyDaZdi-_M9yN9d_kFBv1FBTs03ifASOzfA",
+      authDomain: "trumgiamgia.firebaseapp.com",
+      projectId: "trumgiamgia",
+      storageBucket: "trumgiamgia.appspot.com",
+      messagingSenderId: "847348910930",
+      appId: "1:847348910930:web:2b313fb8051d8d95dfc88b",
+      measurementId: "G-J3E3VC8HGG"
+};
